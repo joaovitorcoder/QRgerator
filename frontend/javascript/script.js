b3538f7138702cd184b9ec33258a1f27e25903e7
João Vitor <olhierturcojoaovitor@gmail.com>
2026-03-22 12:59:46 -0300

    Initial commit

---
+++ frontend/javascript/script.js
@@ -0,0 +1,68 @@
+const qrcodeDiv = document.getElementById("qrcode")
+const input = document.getElementById("input-link")
+
+
+function criar_QR(){
+  //Limpar o campo de código QR
+  qrcodeDiv.innerHTML = ""
+
+  if(!input.value){
+    qrcodeDiv.innerHTML = `<h5 style="color: white;">Impossível a criação de códigoQR</h5>`
+  }else{
+    new QRCode(qrcodeDiv, {
+      text: `https://${input.value}.com`,
+      width: 150,
+      height: 150,
+      colorDark: "#ffffff", // cor do QRcode
+      colorLight: "#000000" //cor do fundo do QRcode 
+    })
+  }
+}
+
+
+//Limpar input e div do qrcode
+function limpar_QR(){
+  qrcodeDiv.innerHTML = ''
+  input.value = ''
+}
+
+/**
+*                                                  ,-.
+*                                               ("O_)
+*                                              / `-/
+*                                             /-. /
+*                                            /   )
+*                                           /   /  
+*                              _           /-. /
+*                             (_)"-._     /   )
+*                               "-._ "-'""( )/    
+*                                   "-/"-._" `. 
+*                                    /     "-.'._
+*                                   /\       /-._"-._
+*                    _,---...__    /  ) _,-"/    "-(_)
+*                ___<__(|) _   ""-/  / /   /
+*                 '  `----' ""-.   \/ /   /
+*                               )  ] /   /
+*                       ____..-'   //   /                       )
+*                   ,-""      __.,'/   /   ___                 /,
+*                  /    ,--""/  / /   /,-""   """-.          ,'/
+*                 [    (    /  / /   /  ,.---,_   `._   _,-','
+*                  \    `-./  / /   /  /       `-._  """ ,-'
+*                   `-._  /  / /   /_,'            ""--"
+*                       "/  / /   /"         
+*                       /  / /   /
+*                      /  / /   /  o!O
+*                     /  |,'   /  
+*                    :   /    /
+*                    [  /   ,'   "Blackadder"
+*                    | /  ,'
+*                    |/,-'
+*                    P'  ____   _                                     
+*   / __ \ (_) ___   __ _  ___  ___ _ ____      __
+*  / / _` || |/ _ \ / _` |/ _ \/ __| '_ \ \ /\ / /
+* | | (_| || | (_) | (_| | (_) \__ \ |_) \ V  V / 
+*  \ \__,_|/ |\___/ \__,_|\___/|___/ .__/ \_/\_/  
+*   \____/__/                      |_|          
+*                  @ j o a o s p w
+*/
+
